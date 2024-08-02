fbe89f940b25af80e470a196b28722af49f98b95
yusufzubeyirur <yusufzubeyirur@gmail.com>
2024-08-02 17:40:36 +0300

    css

---
+++ src/app/App.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import "./styles/App.css";
+
+export default function App() {
+  const [query, setQuery] = useState("");
+  const [users, setUsers] = useState([]);
+
+  const handleSearch = async () => {
+    try {
+      const response = await fetch(
+        `https://api.github.com/search/users?q=${query}`
+      );
+      if (response.ok) {
+        const data = await response.json();
+        setUsers(data.items);
+      } else {
+        setUsers([]); // Kullanıcı bulunamazsa boş array döndür
+      }
+    } catch (error) {
+      setUsers([]); // Hata durumunda da boş array döndür
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>Project 5:GitHub Kullanıcı Arama</h1>
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        className="input"
+      />
+      <button onClick={handleSearch} className="button">
+        Ara
+      </button>
+      {users.length === 0 && query && (
+        <p className="error">Kullanıcı bulunamadı</p>
+      )}
+      <h2>Results</h2>
+      <div className="results">
+        {users.map((user) => (
+          <div key={user.id} className="userCard">
+            <img src={user.avatar_url} alt={user.login} className="avatar" />
+            <div className="userInfo">
+              <h2>{user.login}</h2>
+              <a href={user.html_url} target="_blank" rel="noopener noreferrer">
+                Profile Git
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
